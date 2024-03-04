09df9f28c18ddae5a0661838f0eb50509c6e7440
caio <caioliberal@gmail.com>
2024-03-04 19:41:26 -0300

    Rotas do site

--- app.py
+++ app.py
@@ -2,11 +2,35 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+#Rotaas do site
+
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('./template/html/main.html')
 
+@app.route('/login')
+def login():
+    return render_template('./template/html/login.html')
 
+@app.route('/register')
+def register():
+    return render_template('./template/html/register.html')
+
+@app.route('/evento')
+def eventos():
+    return render_template('./template/html/eventos.html')
+
+@app.route('Bazar')
+def bazar():
+    return render_template('./template/html/bazar.html')
+
+@app.route('faca_parte')
+def faca_parte():
+    return render_template('./template/html/faca_parte.html')
+
+@app.route('historia_para_contar')
+def historia_para_contar():
+    return render_template('./template/html/historia_para_contar.html')
 
 
 if __name__ == '__main__':
