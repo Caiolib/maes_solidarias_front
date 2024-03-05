3c5389b277fe14a210b6036961eeba5b56f97f3c
caio <caioliberal@gmail.com>
2024-03-05 13:55:02 -0300

    Rotas, eventos

--- requisicoes.py
+++ requisicoes.py
@@ -22,7 +22,7 @@ def remover_categoria(id):
 
 #Rotas Produto
 def criar_produto(nome, descricao, preco, estoque, total_vendido, categoria):
-    url = 'http://seu-endereco-django.com/items/'
+    url = 'http://127.0.0.1:8000/bazar/items/'
     headers = {'Content-Type': 'application/json'}
     data = {
         'nome': nome,
@@ -59,4 +59,40 @@ def remover_produto(id):
     url = f'http://127.0.0.1:8000/bazar/items/{id}/'  
     headers = {'Content-Type': 'application/json'}
     response = requests.delete(url, headers=headers)
+    return response.json()
+
+#eventos
+
+
+
+# Função para criar um novo evento
+def criar_evento(data):
+    url = 'http://127.0.0.1:8000/events/'  
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=data, headers=headers)
+    return response.json()
+
+# Função para obter todos os eventos
+def obter_eventos():
+    url = 'http://127.0.0.1:8000/events/'  
+    response = requests.get(url)
+    return response.json()
+
+# Função para obter um evento único pelo ID
+def obter_evento_unico(id):
+    url = f'http://127.0.0.1:8000/events/unique/{id}'  
+    response = requests.get(url)
+    return response.json()
+
+# Função para deletar um evento
+def deletar_evento(id):
+    url = f'http://127.0.0.1:8000/events/unique/{id}'  
+    response = requests.delete(url)
+    return response.json()
+
+# Função para atualizar um evento
+def atualizar_evento(id, data):
+    url = f'http://127.0.0.1:8000/events/unique/{id}'  
+    headers = {'Content-Type': 'application/json'}
+    response = requests.put(url, json=data, headers=headers)
     return response.json()
